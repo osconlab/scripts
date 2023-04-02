e830aef3793640c8068e927d8218af8f0f93eee2
osconlab <129285747+osconlab@users.noreply.github.com>
2023-04-02 21:25:58 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -1,2 +1,10 @@
 apt update && sudo apt upgrade -y
 apt install htop curl -y
+while true; do
+    read -p "Do you wish to install docker" yn
+    case $yn in
+        [Yy]* ) ./docker.sh; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
+done
