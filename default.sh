86108bd1587e1a142f3fd99e5d3f79f6037a0ade
osconlab <129285747+osconlab@users.noreply.github.com>
2023-04-03 22:08:06 +0200

    Update default.sh

--- default.sh
+++ default.sh
@@ -3,7 +3,7 @@ apt update && sudo apt upgrade -y
 apt install htop curl -y
 chmod +x ./docker.sh
 while true; do
-    read -p "Do you wish to install docker: ?" yn
+    read -p "Do you wish to install docker ?:" yn
     case $yn in
         [Yy]* ) ./docker.sh; break;;
         [Nn]* ) exit;;
